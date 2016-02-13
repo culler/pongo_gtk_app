82f8d83042b5070a161f92b20b60eb3a06457f4e
Marc Culler <culler@users.noreply.github.com>
2016-02-13 17:22:30 -0600

    Basically working.

---
+++ pongo/__init__.py
@@ -0,0 +1,13 @@
+class PongoServer:
+    def __init__(self, name, ip_address):
+        self.name = name
+        self.ip_address = ip_address
+        
+    def __eq__(self, other):
+        return self.ip_address == other.ip_address
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return 'Pongo %s (%s)'%(self.name, self.ip_address)
