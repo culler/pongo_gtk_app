b6c41872fd76312168b1b40e75db2f8f27ab2803
Marc Culler <Marc Culler@localhost>
2016-02-14 09:58:37 -0600

    Gtk app is working.  Detects and opens Pongos.  Closing the browser kills the application, but the finder window can be brought to the front to change Pongos.

--- pongo/app.py
+++ pongo/app.py
@@ -2,6 +2,7 @@ import sys, signal
 from gi.repository import Gtk, Gdk, GObject
 from . import PongoServer
 from .find_pongo import FindPongo
+from .play_pongo import PlayPongo
 from .style import css
 
 style = Gtk.CssProvider()
@@ -11,33 +12,48 @@ Gtk.StyleContext.add_provider_for_screen(
     style,     
     Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
 )
-                     
+
 class PongoApplication(Gtk.Application):
-    _servers = []
+    servers = []
     
     def __init__(self):
         Gtk.Application.__init__(self)
         self.window = None
-        self.grid = None
+        self.player = None
                            
     def do_activate(self):
         if not self.window:
             self.window = window = Gtk.ApplicationWindow(application=self,
                                                 title="Pongo")
             window.set_size_request(400, 600)
+            window.move(75, 50)
             window.set_border_width(10)
-            self.find_pongo = find_pongo = FindPongo(self._servers)
-            window.add(find_pongo)
+            self.finder = finder = FindPongo(self)
+            window.add(finder)
         self.window.present()
 
+    def play_pongo(self, server):
+        if not self.player:
+            self.player = player = PlayPongo(self, server)
+            player.move(75, 75)
+        else:
+            self.player.load(server)
+        self.player.show_all()
+        self.window.iconify()
+
+    def player_destroyed(self, widget):
+        self.shutdown()
+        self.quit()
+        
     def do_startup(self):
         Gtk.Application.do_startup(self)
 
     def shutdown(self):
-        self.find_pongo.shutdown()
+        if self.finder:
+            self.finder.shutdown()
         
 if __name__ == '__main__':
-    
+
     GObject.threads_init()
     app = PongoApplication()
     signal.signal(signal.SIGINT, signal.SIG_DFL)
